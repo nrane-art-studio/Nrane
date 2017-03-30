f5e1dbfdef498df84025747321307235dc4dd9e8
nrane-art-studio <nrane.art@gmail.com>
2017-03-30 12:52:55 +0400

    4

--- data.php
+++ data.php
@@ -1,21 +1,5 @@
 <?php
 
-<<<<<<< HEAD
-$conn = new mysqli($servername, $username, $password, $dbname);
-    // Check connection
-    if ($conn->connect_error) {
-        die("Connection failed: " . $conn->connect_error);
-}
-
-
-$fname = $_POST["fname"];
-$lname = $_POST["lname"];
-$mail = $_POST["mail"];
-
-if (isset ($_POST["fname"] && $_POST["lname"] && $_POST["mail"])) {
-
-} else
-=======
 $connect = new mysqli('localhost', 'root', '', 'nrane');
     // Check connection
     if ($connect->connect_error) {
@@ -24,7 +8,7 @@ $connect = new mysqli('localhost', 'root', '', 'nrane');
 }
 
 // charset
-mysqli_query($connect, "SET NAMES UTF8");
+//mysqli_query($connect, "SET NAMES UTF8");
 // time zone
 mysqli_query($connect, "SET time_zone = '+04:00'");
 
@@ -33,33 +17,29 @@ $lname = $_POST['lname'];
 $mail = $_POST['mail'];
 
 if ($_POST["fname"] && $_POST["lname"] && $_POST["mail"]) {
-    echo " Անուն - ".$fname."<br>";
-    echo " Ազգանուն - ".$lname."<br>";
-    echo " Էլ․ հասցե - ".$mail."<hr>";
-    $koko = "CREATE TABLE IF NOT EXISTS `test` (
-            name varchar(50)
+//    echo " Անուն - ".$fname."<br>";
+//    echo " Ազգանուն - ".$lname."<br>";
+//    echo " Էլ․ հասցե - ".$mail."<hr>";
+    $koko = "CREATE TABLE IF NOT EXISTS `test1` (
+            id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+            firstname VARCHAR(30) NOT NULL,
+            lastname VARCHAR(30) NOT NULL,
+            email VARCHAR(50),
     )";
     if ($connect->query($koko)) {
-        echo "Դուք հաջողությամբ բաժանորդագրվել եք։"."<br>";
+//        echo "Դուք հաջողությամբ բաժանորդագրվել եք։"."<br>";
     } else {
-        echo "Չեղավ!!!";
+        echo "Չեղավ  324r!!!<br><br>";
     };
-    $lolo = "INSERT INTO test (name) VALUES ('$fname')";
+    $lolo = "INSERT INTO test1 (firstname, lastname, email) VALUES ('$fname', '$lname', '$mail')";
 
     if ($connect->query($lolo)) {
-        echo "Դուք հաջողությամբ բաժանորդագրվել եք։"."<br>";
+//        echo "Դուք հաջողությամբ բաժանորդագրվել եք։"."<br>";
     } else {
-        echo "Չեղավ!!!";
+        echo "Չեղավ!!!<hr>";
     }
-
-
 
 } else {
     echo "Սխալ կա";
 };
-<<<<<<< HEAD
 ?>
-=======
->>>>>>> origin/master
-?>
->>>>>>> origin/master
